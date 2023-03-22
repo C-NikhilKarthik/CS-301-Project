e369f1b0be6cb88607867e50c08fad62f981ccd5
nikhilkarthik <nikhilkarthik241103@gmail.com>
2023-03-22 12:33:03 +0000

    feat: server folder structured

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.TheBlogPenn.com',
+    'https://TheBlogPenn.com'
+]
+
+module.exports = allowedOrigins
--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
-const express=require('express')
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const { logger, logEvents } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3500
+
 const {connectToDb,getDb}=require('./db')
-const app=express()
-const path=require('path')
 app.use(express.urlencoded({extended:false}))//to be able to read data from the url or form which we send
 app.use(express.json())
 
@@ -143,7 +153,6 @@ app.get('/explore',(req,res)=>{
 
 })
 
-
 // app.post('/create_blog',(req,res)=>{
 //     // console.log(req.body)
 //     // const {Id,post_text}=req.body
@@ -154,3 +163,45 @@ app.get('/explore',(req,res)=>{
 
 // })
 
+// course
+
+console.log(process.env.NODE_ENV)
+
+connectDB()
+
+app.use(logger)
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/', require('./routes/root'))
+app.use('/users', require('./routes/userRoutes'))
+app.use('/notes', require('./routes/noteRoutes'))
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
