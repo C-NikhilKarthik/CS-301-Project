24e0c4590587f437775253802c9f27a323a0b60b
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2023-04-18 01:14:37 +0530

    feat: Tags

--- client/src/Components/Home/ProfileCard.jsx
+++ client/src/Components/Home/ProfileCard.jsx
@@ -30,18 +30,18 @@ function ProfileCard({ url }) {
     const json = await response.json();
     var followers = 0;
     const id = json._id;
-    // for (let i = 0; i < json2.length; i++) {
-    //   if (json2[i].Friends.length === 0) {
-    //     continue;
-    //   }
-    //   else {
-    //     for (let j = 0; j < json2[i].Friends.length; j++) {
-    //       if (json2[i].Friends[j] === id) {
-    //         followers++;
-    //       }
-    //     }
-    //   }
-    // }
+    for (let i = 0; i < json2.length; i++) {
+      if (json2[i].Friends.length === 0) {
+        continue;
+      }
+      else {
+        for (let j = 0; j < json2[i].Friends.length; j++) {
+          if (json2[i].Friends[j] === id) {
+            followers++;
+          }
+        }
+      }
+    }
 
     setData({ name: json.UserName, following: (json.Friends.length > 0) ? json.Friends.length : 0, followers: followers });
     console.log(data)
