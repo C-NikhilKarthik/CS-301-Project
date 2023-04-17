a7226fe65cb6b96e8fe28bf2e8b0e85a563d5192
Avaneesh Sundararajan <avaneeshsundar@gmail.com>
2023-04-17 23:03:34 +0530

    feat: read the blog content out

--- client/src/pages/BlogSlug.jsx
+++ client/src/pages/BlogSlug.jsx
@@ -11,6 +11,8 @@ function BlogSlug() {
   const [content, SetContent] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [home_url, setHomeUrl] = useState("")
+  const [textToRead, setTextToRead] = useState("");
+
   const generate_blogs = async (e) => {
     setIsLoading(true);
     const temp_list = [];
@@ -43,10 +45,59 @@ function BlogSlug() {
     //Setlist(temp_list);
   };
 
+  //read speech:
+  const [synthesis, setSynthesis] = useState(null);
+const [isPlaying, setIsPlaying] = useState(false);
+function startSpeech(text) {
+  const utterance = new SpeechSynthesisUtterance(text);
+  const synth = window.speechSynthesis;
+
+  synth.cancel();
+  synth.speak(utterance);
+  setIsPlaying(true);
+  setSynthesis(synth);
+}
+
+function stopSpeech() {
+  synthesis.cancel();
+  setIsPlaying(false);
+}
+const [html, setHtml] = useState(content);
+const [text, setText] = useState("");
+
+const handleSpeechButtonClick = () => {
+  if (!isPlaying) {
+    startSpeech(text);
+  } else {
+    stopSpeech();
+  }
+};
+
+  function getTextFromHtml(html,callback) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    let text = "";
+  
+    const treeWalker = doc.createTreeWalker(doc.body, NodeFilter.SHOW_TEXT, null, false);
+    while (treeWalker.nextNode()) {
+      text += treeWalker.currentNode.textContent.trim() + " ";
+    }
+  
+    callback(text);
+  }
+
+  // const handleSpeakButtonClick = () => {
+  //   const text = getTextFromHtml(content);
+
+  // const utterance = new SpeechSynthesisUtterance(text);
+  // window.speechSynthesis.speak(utterance);
+  // };
+
   console.log(content);
   useEffect(() => {
     generate_blogs();
-  }, []);
+    getTextFromHtml(content, setText);
+  }, [html]);
   return (
     <>
       {isLoading ? (<Loading />) : (
@@ -56,6 +107,9 @@ function BlogSlug() {
         </div>
         <div className="w-full flex flex-col items-center gap-6 p-6 mx-auto sm:w-4/5 lg:2/3 sm:px-16 mb-16 md:px-24 lg:px-32">
           <div dangerouslySetInnerHTML={{ __html: content }} className="rounded-md w-full ql-editor bg-slate-300/40 dark:bg-slate-800/40 backdrop-blur-md text-slate-800 dark:text-slate-200"></div>
+          <button onClick={handleSpeechButtonClick} className="text-white">
+      {isPlaying ? "Stop" : "Speak"}
+    </button>
           <Comments />
         </div>
         <Footer />
